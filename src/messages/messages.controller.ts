1a90d2ef3e0467c5c234b59d0dd69b08afe010d9
Ankit <ankit.dobhal@forto.com>
2022-04-29 07:32:35 +0200

    Some more changes

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { get } from 'http';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  //   @Get()
+  //   listMessages() {}
+
+  @Post()
+  createMessages(@Body() body: CreateMessageDto) {
+    console.log(body);
+  }
+
+  @Get('/:id')
+  getMessage(@Param() id: string) {
+    console.log(id);
+  }
+}
