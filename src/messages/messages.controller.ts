1f647b5fedcc0824fb61f8f7d5cbbbf901d663eb
Ankit <ankit.dobhal@forto.com>
2022-05-07 08:06:00 +0200

    Add dependecy injection

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,19 +1,35 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
-import { get } from 'http';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesServices } from './messages.services';
 
 @Controller('messages')
 export class MessagesController {
-  //   @Get()
-  //   listMessages() {}
+  constructor(public messagesService: MessagesServices) {}
+
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
   createMessages(@Body() body: CreateMessageDto) {
-    console.log(body);
+    this.messagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessage(@Param() id: string) {
-    console.log(id);
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+    return message;
   }
 }
